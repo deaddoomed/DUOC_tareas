88c53a971673ddfbfca2f13a3db31b452f0dae39
deaddoomed <igna.gallardo@duocuc.cl>
2022-06-05 22:49:01 -0400

    segunda version

--- tarea_funciones.py
+++ tarea_funciones.py
@@ -7,4 +7,34 @@ def descuento(producto,porcentaje):
 
 def imc(peso,altura):
   indice=peso*altura*altura
-  if rango i wea 
+  if indice>=40:
+    print("Obesidad Grado 3")
+  elif indice>=35 and indice<40:
+    print("Obesidad Grado 2")
+  elif indice>=30 and indice<35:
+    print("Obesidad Grado 1")
+  elif indice>=25 and indice<30:
+    print("Sobrepeso")
+  elif indice>=18.5 and indice<25:
+    print("Adecuado")
+  else:
+    print("Bajo Peso")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
