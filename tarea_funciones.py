5cc1c2d2b4d91f007b7aea40e4d2a4d2c0244523
deaddoomed <igna.gallardo@duocuc.cl>
2022-06-05 22:42:12 -0400

    primera version

---
+++ tarea_funciones.py
@@ -0,0 +1,10 @@
+def calcular_iva(precio):
+  return precio_final=precio*1,19
+
+def descuento(producto,porcentaje):
+  valor=producto*(100-porcentaje)/100
+  print("el valor final del producto: $",valor)
+
+def imc(peso,altura):
+  indice=peso*altura*altura
+  if rango i wea 
